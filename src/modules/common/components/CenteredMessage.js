a84fc9919e5e94bc052aaca4a734e11efe1e34d9
Edward Jimenez <edwardjimenez@Edwards-Mac-mini.local>
2019-10-07 13:58:52 -0500

    rs: refactored into a functional component

--- src/modules/common/components/CenteredMessage.js
+++ src/modules/common/components/CenteredMessage.js
@@ -1,73 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Close } from '@material-ui/icons';
 import '../styles/CenteredMessage.css';
 
-export default class CenteredMessage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: true,
-    };
-  }
+// In case the message needs render the Close button or not
+const renderCloseButton = (showClose, isOpen, open) => (showClose ? (
+  <button className="closeButton" type="button" onClick={() => isOpen(!open)}>
+    <Close />
+  </button>
+) : null
+);
 
-  // The method handles the close button
-  hideMessage() {
-    this.setState({ isOpen: false });
-  }
+// This method handles the breaks lines in the message string provided.
+const renderMessage = (message) => {
+  const parsedMessage = message.toString().split('\n').map((item, key) => (
+    <span key={`message-${key + 1}`}>
+      {item}
+      <br />
+    </span>
+  ));
 
-  // In case the message needs render the Close button or not
-  renderCloseButton() {
-    const { showClose } = this.props;
+  return parsedMessage;
+};
 
-    return (showClose ? (
-      <button className="closeButton" type="button" onClick={() => this.hideMessage()}>
-        <Close />
-      </button>
-    ) : null
-    );
-  }
+export default function CenteredMessage({
+  showClose,
+  title,
+  buttonText,
+  message,
+  onContinue,
+}) {
+  const [open, isOpen] = useState(true);
 
-  // This method handles the breaks lines in the message string provided.
-  renderMessage() {
-    const { message } = this.props;
-
-    const parsedMessage = message.toString().split('\n').map((item, key) => (
-      <span key={`message-${key + 1}`}>
-        {item}
-        <br />
-      </span>
-    ));
-
-    return parsedMessage;
-  }
-
-  // The method handles the main render of the message Component
-  renderComponent() {
-    const {
-      title,
-      buttonText,
-      onContinue,
-    } = this.props;
-
-    return (
-      <div className="container">
-        <div className="divStyle">
-          { this.renderCloseButton() }
-          <h1 className="titleStyle">{ title }</h1>
-          <p className="messageStyle">{ this.renderMessage() }</p>
-          <button className="buttonStyle" type="button" onClick={onContinue}>
-            { buttonText }
-          </button>
-        </div>
+  return open ? (
+    <div className="container">
+      <div className="divStyle">
+        { renderCloseButton(showClose, isOpen, open) }
+        <h1 className="titleStyle">{ title }</h1>
+        <p className="messageStyle">{ renderMessage(message) }</p>
+        <button className="buttonStyle" type="button" onClick={onContinue}>
+          { buttonText }
+        </button>
       </div>
-    );
-  }
-
-  render() {
-    const { isOpen } = this.state;
-    return isOpen ? this.renderComponent() : null;
-  }
+    </div>
+  ) : null;
 }
 
 // The PropTypes for the props received
